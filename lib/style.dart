00d2dce7c4e5695f9b61655b4f8f3583425618f2
DouglassSpencer <douglas.spencer02@gmail.com>
2020-06-12 10:03:20 -0300

    Página de inserção da url

--- lib/style.dart
+++ lib/style.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'algorithmPage.dart';
 
-Widget mainButtonStyle(int color, String title){
+Widget mainButtonStyle(int color, String title) {
   Color color1, color2;
 
   if (color == 1) {
@@ -31,38 +31,37 @@ Widget mainButtonStyle(int color, String title){
     color2 = new Color(0xFF00F260);
   }
 
-  return Container(//botoes da homepage
+  return Container(
+    //botoes da homepage
     margin: EdgeInsets.all(10),
     decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(30),
         gradient: LinearGradient(colors: [color1, color2]),
-        boxShadow: <BoxShadow>[BoxShadow(
-            color: Colors.black,
-            offset: Offset(1.0, 6.0),
-            blurRadius: 40.0)]
-  ),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+              color: Colors.black, offset: Offset(1.0, 6.0), blurRadius: 40.0)
+        ]),
     height: 80.0,
     child: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              title.toUpperCase(),
-              style: TextStyle(
-                  fontFamily: 'Josefin Sans',
-                  color: Colors.white,
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w200,
-                  letterSpacing: 4,)
-
-            )
-          ],
-        )),
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(title.toUpperCase(),
+            style: TextStyle(
+              fontFamily: 'Josefin Sans',
+              color: Colors.white,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w200,
+              letterSpacing: 4,
+            ))
+      ],
+    )),
   );
 }
 
 //PAGINA DOS ALGORITMOS
-Widget algorithmPageStyle(List<String> _image, File _originalImage, String title, String algorithm){
+Widget algorithmPageStyle(
+    List<String> _image, File _originalImage, String title, String algorithm) {
   return Scaffold(
       backgroundColor: Colors.white,
       appBar: new AppBar(
@@ -73,10 +72,53 @@ Widget algorithmPageStyle(List<String> _image, File _originalImage, String title
           title,
           style: TextStyle(color: Colors.white),
         ),
-
       ),
       body: Container(
-          margin: EdgeInsets.only(left:15.0,top:10.0,right:15.0,bottom: 5.0),
-          child:algorithmPage(_image, _originalImage, algorithm))
-  );
+          margin:
+              EdgeInsets.only(left: 15.0, top: 10.0, right: 15.0, bottom: 5.0),
+          child: algorithmPage(_image, _originalImage, algorithm)));
+}
+
+
+TextEditingController _urlController = new TextEditingController();
+
+Widget urlPageStyle() {
+String text;
+if (_urlController.text == null) text = "URL já inserida: nenhuma.";
+else text = "URL já inserida: " + _urlController.text;
+  return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: new AppBar(
+        elevation: 10.0,
+        centerTitle: true,
+        backgroundColor: Color(0xFF283593),
+        title: new Text(
+          "URL",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Container(
+          margin:
+              EdgeInsets.only(left: 15.0, top: 10.0, right: 15.0, bottom: 5.0),
+          child: Column(
+            children: <Widget>[
+              Text("Insira a URL do sevidor", style: TextStyle(fontSize: 16.0),),
+              Padding(padding: EdgeInsets.all(10.0),),
+              TextField(
+                controller: _urlController,
+                autofocus: true,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    icon: Icon(Icons.web),
+                    hintText: 'URL'),
+              ),
+              Padding(padding: EdgeInsets.all(10.0),),
+              Text(text, style: TextStyle(fontSize: 16.0),),
+            ],
+          )
+          //child:
+          ));
+}
+TextEditingController getUrl(){
+  return _urlController;
 }
